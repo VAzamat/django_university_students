2d6f2bae43ae7d32463e541d4ef10d55fc7087a0
Valeev Azamat <azamat@mail.sao.ru>
2025-03-20 19:33:50 +0300

    students app: model Student created, db access settings

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 - skypro_python41.0
 - студенты
 
+### Эволюция проекта
+- [x] Классная работа "20.1 Работа с ORM (Object-Relational Mapping) в Django"
+### Шаг за шагом
 
+#### 20.1 Работа с ORM (Object-Relational Mapping) в Django
+
+ - Настройка работы с базой данных  СУБД PostgreSQL
+ - Настраиваем пути в urls.py, MEDIA_URL и MEDIA_ROOT
--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":     os.environ.get('POSTGRES_DATABASE_NAME'),
+        "USER":     os.environ.get('POSTGRES_DATABASE_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_DATABASE_USER_PASSWORD'),
+        "HOST":     os.environ.get('POSTGRES_DATABASE_HOST'),
+        "PORT":     os.environ.get('POSTGRES_DATABASE_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
