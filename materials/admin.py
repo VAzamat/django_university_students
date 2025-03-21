85b895b461ebbf58c811d018ab766136aa7f8d6a
Valeev Azamat <azamat@mail.sao.ru>
2025-03-21 20:13:04 +0300

    materials app: create admin cabinet for app

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from materials.models import Material
+from students.templatetags.path_tag_filter import register
 
 # Register your models here.
+
+@admin.register(Material)
+class MaterialClass(admin.ModelAdmin):
+    list_display = ("title","body",)
+    search_fields = ("title",)
+
+
