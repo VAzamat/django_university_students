050602ae1cbcf6faa4d2f77db487d104068c419f
Valeev Azamat <azamat@mail.sao.ru>
2025-03-21 20:12:28 +0300

    materials app: create model *Material* with *title* and *body*

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Material(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название",
+        help_text="Введите название учебного курса"
+    )
+    body = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание учебного курса"
+    )
+
+    def __str__(self):
+        return f'Курс: {self.title}\nОписание: {self.body}\n'
+
+    class Meta:
+        verbose_name = "Материал"
+        verbose_name_plural = "Материалы"
