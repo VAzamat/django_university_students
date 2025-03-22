a9979739fcf6cab0d20aebdcb286371153d9373d
Valeev Azamat <azamat@mail.sao.ru>
2025-03-22 22:00:44 +0300

    students app: CRUD (CreateReadUpdateDelete) for *Student* was realized

--- students/urls.py
+++ students/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from students.views import student_list, student_detail
+from students.views import StudentDetailView, StudentListView, StudentUpdateView, StudentCreateView, StudentDeleteView
 
 from students.apps import StudentsConfig
 
 app_name = StudentsConfig.name
 
 urlpatterns = [
-    path("", student_list, name='student_list'),
-    path("student/<int:pk>/", student_detail, name='student_detail'),
+    path("",                  StudentListView.as_view(), name='list'),
+    path("student/<int:pk>/", StudentDetailView.as_view(), name='detail'),
+    path("student/create/", StudentCreateView.as_view(), name='create'),
+    path("student/update/<int:pk>/", StudentUpdateView.as_view(), name='update'),
+    path("student/delete/<int:pk>/", StudentDeleteView.as_view(), name='delete'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.urls import reverse_lazy, reverse
+from django.views.generic import  CreateView, ListView, DetailView, UpdateView, DeleteView
 from students.models import Student
+
+
 
 # Create your views here.
 
-def student_list(request):
-    students = Student.objects.all()
+class StudentDetailView(DetailView):
+    model = Student
+    #template_name = "students/student_detail.html"
 
-    context = {
-        'student_list': students
-    }
+class StudentListView(ListView):
+    model = Student
+    #template_name = "students/student_list.html"
 
-    return render(request, 'students/student_list.html', context)
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ("last_name", 'first_name', "patronymic", "is_active", "avatar")
+    success_url = reverse_lazy('students:list')
 
-def student_detail(request, pk):
-    student = get_object_or_404(Student,pk=pk)
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ("last_name", 'first_name', "patronymic", "is_active", "avatar")
+    def get_success_url(self):
+        return reverse('students:detail', args=[self.kwargs.get('pk')] )
 
-
-    context = {
-        'student_item': student
-    }
-
-    return render(request, 'students/student_detail.html', context)
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students:list')
