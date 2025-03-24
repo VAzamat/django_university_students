fdec05b49a820c17436274f6cec3dd65da3301ba
Valeev Azamat <azamat@mail.sao.ru>
2025-03-25 00:16:53 +0300

    students app: validate/check email in clean_email function

--- students/forms.py
+++ students/forms.py
@@ -9,3 +9,11 @@ class StudentForm(forms.ModelForm):
         model = Student # Обязательно указываем модель
         #fields = '__all__' # и перечисляем поля для отображения
         fields = ('first_name', 'last_name', 'avatar', 'email', )
+
+    def clean_email(self):
+        cleaned_data = self.cleaned_data['email']
+
+        if 'mail.ru' not in cleaned_data:
+            raise forms.ValidationError('Почта должна быть на домене mail.ru')
+
+        return cleaned_data
